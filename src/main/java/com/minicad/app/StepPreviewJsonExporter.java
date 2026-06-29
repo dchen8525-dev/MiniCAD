a34b33932903bd9cfe30b55a1c799b54aea8ffe0
dewei <860656812@qq.com>
2026-06-29 19:54:40 +0800

    Refactor: Extract ParametricSurfaceMapper interface to separate file

--- src/main/java/com/minicad/app/StepPreviewJsonExporter.java
+++ src/main/java/com/minicad/app/StepPreviewJsonExporter.java
@@ -17112,22 +17112,6 @@ public final class StepPreviewJsonExporter {
     }
 
 
-    private interface ParametricSurfaceMapper {
-        UvPoint project(CartesianPoint point, UvPoint previous);
-
-        CartesianPoint pointAt(double u, double v);
-
-        Vector3 normalAt(double u, double v);
-
-        default Double uPeriod() {
-            return null;
-        }
-
-        default Double vPeriod() {
-            return null;
-        }
-    }
-
     private interface CurveEvaluator {
         double start();
 
